4df033ac4c3edea6afb3be18e08e133b4d6e17ae
AleksandarUsljebrka <usljebrkaaleksandar@gmail.com>
2024-02-11 14:29:22 +0100

    Added FoodController; added DatabaseContext ; style changes

---
+++ NutritionApp/Controllers/FoodController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NutritionApp.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddFood()
+        {
+            return View();
+        }
+    }
+}
