8d068e653ddb04a2ee7e2f9929bf9ba1ae45a44d
AleksandarUsljebrka <usljebrkaaleksandar@gmail.com>
2024-02-13 11:02:30 +0100

    Added form validation

--- NutritionApp/Controllers/FoodController.cs
+++ NutritionApp/Controllers/FoodController.cs
@@ -4,30 +4,55 @@ using NutritionApp.Models;
 
 namespace NutritionApp.Controllers
 {
-    public class FoodController : Controller
-    {
-        private ApplicationDbContext _context;
-        public FoodController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-        public IActionResult Index()
-        {
-            List<Food> foods = _context.Food.ToList();
-            return View(foods);
-        }
+	public class FoodController : Controller
+	{
+		private ApplicationDbContext _context;
+		public FoodController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			List<Food> foods = _context.Food.ToList();
+			return View(foods);
+		}
 
-        public IActionResult AddFood()
-        {
-            return View();
-        }
+		public IActionResult AddFood()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public IActionResult AddFood(Food newFood)
-        {
-            _context.Add(newFood);
-            _context.SaveChanges();
-            return View();
-        }
-    }
-} 
+		[HttpPost]
+		public IActionResult AddFood(Food newFood)
+		{
+			Dictionary<string, string> valueMappings = new Dictionary<string, string>
+			{
+				{ "meatFishEggs", "Meat, Fish and eggs" },
+				{ "dairy", "Dairy product" },
+				{ "fruit", "Fruits" },
+				{ "vegetables", "Vegetables" },
+				{ "grains", "Grains" },
+				{ "legumes", "Legumes" },
+				{ "nuts", "Nuts and Seeds" },
+				{ "oils", "Oils and Fats" },
+				{ "sweets", "Sweets and Snacks" },
+				{ "beverages", "Beverages" },
+
+			};
+
+			
+			if (ModelState.IsValid)
+			{
+				if (valueMappings.ContainsKey(newFood.TypeOfFood))
+				{
+					newFood.TypeOfFood = valueMappings[newFood.TypeOfFood];
+
+				}
+				_context.Add(newFood);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+	}
+}
--- NutritionApp/Models/Food.cs
+++ NutritionApp/Models/Food.cs
@@ -8,20 +8,21 @@ namespace NutritionApp.Models
         [Key] 
         public int Id { get; set; }
         
-        [Required]
         [DisplayName("Food Name")]
+        [StringLength(25,MinimumLength =2)]
+
+		[RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Only characters are alowed!")]
+		[Required]
         public string FoodName { get; set; }
         [Required]
         [DisplayName("Type of Food")]
         public string TypeOfFood { get; set; }
-        [Required]
-        [DisplayName("Proteins")]
+        [Required(ErrorMessage ="This field is required!")]
         public float Proteins { get; set;}
-        [Required]
-        [DisplayName("Carbs")]
-        public float Carbs { get; set;}
-        [Required]
-        [DisplayName("Fat")]
+		[Required(ErrorMessage = "This field is required!")]
+		public float Carbs { get; set;}
+        [Required(ErrorMessage ="This field is required!")]
+
         public float Fat{ get; set;}
 
     }
